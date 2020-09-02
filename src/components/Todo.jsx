f57041f530fd5aa17a2a8c2003ed39f43c84aafe
manggong <jihwank1995@gmail.com>
2020-09-02 12:41:22 +0900

    Modify Todo.jsx

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-class Todo extends Component {
+export default class Todo extends Component {
   render() {
     return (
       <li
@@ -21,5 +21,3 @@ Todo.propTypes = {
   completed: PropTypes.bool.isRequired,
   text: PropTypes.string.isRequired
 };
-
-export default Todo;
